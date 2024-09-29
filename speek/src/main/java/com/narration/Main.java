8b825db33b37047d73d1052730e2fa1baba1e360
CamronMartin <cbrycem02@gmail.com>
2024-09-29 09:58:23 -0400

    present

--- speek/src/main/java/com/narration/Main.java
+++ speek/src/main/java/com/narration/Main.java
@@ -2,6 +2,6 @@ package com.narration;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Narriator.playSound("Je mapell Best Group!");
     }
 }
