050af1f8215372b6aa8135e172d8bb249bc6191c
aryananand0 <a.aryansurya007@gmail.com>
2024-10-04 00:00:15 -0400

    added the TrueFalse stub methods | fixed issue #31

--- speek/src/main/java/model/TrueFalse.java
+++ speek/src/main/java/model/TrueFalse.java
@@ -1,6 +1,35 @@
 package model;
 
 public class TrueFalse implements QuestionType {
+    private String question;
+    private String answer;
+
+    public TrueFalse(String question, String answer) {
+        //Needs to be implemented
+    }
+    @Override
+    public String getAnswer() {
+        //Needs to be implemented
+        throw new UnsupportedOperationException("Unimplemented method 'getAnswer'");
+    }
+
+    @Override
+    public String getQuestion() {
+        //Needs to be implemented
+        throw new UnsupportedOperationException("Unimplemented method 'getQuestion'");
+    }
+
+    @Override
+    public boolean isCorrect() {
+        //Needs to be implemented
+        throw new UnsupportedOperationException("Unimplemented method 'isCorrect'");
+    }
+
+    @Override
+    public String toString() {
+        //Needs to be implemented
+        return super.toString();
+    }
 
     
 }
