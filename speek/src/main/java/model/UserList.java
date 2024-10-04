fe05da37ad1fd72058c6d32ef4323f9c92a4a0f9
ZachFlores <zjflores@email.sc.edu>
2024-10-04 11:59:28 -0400

    Added UserList class stub, fixed issue #27

---
+++ speek/src/main/java/model/UserList.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.ArrayList;
+
+public class UserList {
+
+    // Attributes
+    private static UserList userList;
+    private ArrayList<User> users;
+
+    // Private constructor 
+    private UserList() {
+        users = new ArrayList<>();
+    }
+
+    // Methods, need to be implemented
+    public static UserList getInstance() {
+        if (userList == null) {
+            userList = new UserList();
+        }
+        return userList;
+    }
+
+    // Method to get a user by a keyword, needs implementation
+    public User getUser(String keyword) {
+        // Search and return the user matching the keyword 
+        for (User user : users) {
+            // Implement  search logic here
+            if (user.getName().equalsIgnoreCase(keyword)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    
+}
