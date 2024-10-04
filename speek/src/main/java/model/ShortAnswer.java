8e54ebf8e8f88480c0af6ba5e9c0a69d8e476006
aryananand0 <a.aryansurya007@gmail.com>
2024-10-04 00:15:04 -0400

    added the ShortAnswer stub methods | fixed issue #35

--- speek/src/main/java/model/ShortAnswer.java
+++ speek/src/main/java/model/ShortAnswer.java
@@ -1,6 +1,34 @@
 package model;
 
 public class ShortAnswer implements QuestionType{
+    private String question;
+    private String answer;
 
+    public ShortAnswer(String question, String answer) {
+        //Needs to be implemented
+    }
+    @Override
+    public String getAnswer() {
+        // Needs to be implemented
+        throw new UnsupportedOperationException("Unimplemented method 'getAnswer'");
+    }
+
+    @Override
+    public String getQuestion() {
+        // Needs to be implemented
+        throw new UnsupportedOperationException("Unimplemented method 'getQuestion'");
+    }
+
+    @Override
+    public boolean isCorrect() {
+        // Needs to be implemented
+        throw new UnsupportedOperationException("Unimplemented method 'isCorrect'");
+    }
+
+    @Override
+    public String toString() {
+        // Needs to be implemented
+        return super.toString();
+    }
 
 }
