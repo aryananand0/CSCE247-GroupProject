acbc94c7154c82f87a5fdff1dc5ea7d0dbff11e8
aryananand0 <a.aryansurya007@gmail.com>
2024-10-04 00:31:34 -0400

    added the MultipleChoice stub methods | fixed issue #40

--- speek/src/main/java/model/MultipleChoice.java
+++ speek/src/main/java/model/MultipleChoice.java
@@ -1,5 +1,37 @@
 package model;
 
+import java.util.ArrayList;
+
 public class MultipleChoice implements QuestionType{
+    private String question;
+    private ArrayList<String> questionOptions;
+    private String answer;
+
+    public MultipleChoice(String question, ArrayList<String> questionOptions, String answer) {
+        // Needs to be implemented
+    }
+    @Override
+    public String getAnswer() {
+        // Needs to be implemented
+        throw new UnsupportedOperationException("Unimplemented method 'getAnswer'");
+    }
+
+    @Override
+    public String getQuestion() {
+        // Needs to be implemented
+        throw new UnsupportedOperationException("Unimplemented method 'getQuestion'");
+    }
+
+    @Override
+    public boolean isCorrect() {
+        // Needs to be implemented
+        throw new UnsupportedOperationException("Unimplemented method 'isCorrect'");
+    }
+
+    @Override
+    public String toString() {
+        // Needs to be implemented
+        return super.toString();
+    }
     
 }
