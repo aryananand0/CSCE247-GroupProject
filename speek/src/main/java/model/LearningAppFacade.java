567456ef1dc1ab37d5e3db895e49c099b683eab9
CamronMartin <cbrycem02@gmail.com>
2024-10-15 20:02:48 -0400

    Created logic to register users, and to the information to the Users.json file. Issue #95

--- speek/src/main/java/model/LearningAppFacade.java
+++ speek/src/main/java/model/LearningAppFacade.java
@@ -1,5 +1,14 @@
 package model;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.UUID;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import java.util.ArrayList;
 
 public class LearningAppFacade {
@@ -9,6 +18,7 @@ public class LearningAppFacade {
     // private User currentUser;
     private UserList user;
     private CourseList course;
+    private static final String USER_FILE_PATH = "/json/User.json";
 
     // Constructor
     public LearningAppFacade() {
@@ -20,11 +30,47 @@ public class LearningAppFacade {
 
     // Registers a new user with email and password, returns the created User object
     public boolean registerUser(String username, String firstName, String lastName,String email, String password) {
-        if(!user.haveUser(username)){
-            return user.addUser(username, firstName, lastName, email, password);
+        if(!user.haveUser(username)) {
+            boolean isUserAdded = user.addUser(username, firstName, lastName, email, password);
+            if(isUserAdded) {
+                addUserToJson(username, firstName, lastName, email, password);
+                return true;
+            }
+        }  
+        return false;
+    }
+
+    private void addUserToJson(String username, String firstName, String lastName, String email, String password) {
+        try {
+            String s = new String(Files.readAllBytes(Paths.get(USER_FILE_PATH)));
+            JSONArray usersArray = new JSONArray(s);
+
+            // Create a new user
+            JSONObject newUser = new JSONObject();
+            newUser.put("userId", UUID.randomUUID().toString());
+            newUser.put("userName", username);
+            newUser.put("firstName", firstName);
+            newUser.put("lastName", lastName);
+            newUser.put("email", email);
+            newUser.put("password", password);
+            newUser.put("progress", 0);
+            newUser.put("dailyReminder", false);
+            newUser.put("favoriteLanguages", new JSONArray());
+
+            // Start each new user with a fresh wipe
+            newUser.put("currentCourses", new JSONArray());
+            newUser.put("achievments", new JSONArray());
+            newUser.put("questionHistory", new JSONArray());
+            newUser.put("currentQuestion", JSONObject.NULL);
+
+            usersArray.put(newUser);
+
+            try (FileWriter file = new FileWriter(USER_FILE_PATH)) {
+                file.write(usersArray.toString(4));
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
         }
-        
-        return false;  
     }
 
     // Logs in a user with email and password, returns the User object if successful
