465e912a08500d9f515a25ab9cd7f0eaa103eba9
CamronMartin <cbrycem02@gmail.com>
2024-10-24 09:32:13 -0400

    Added a method to add Achievments to the array list. #125

--- speek/src/main/java/model/AchievementList.java
+++ speek/src/main/java/model/AchievementList.java
@@ -1,5 +1,34 @@
 package model;
 
+import java.util.ArrayList;
+
 public class AchievementList {
+
+    // Attributes
+    private ArrayList<Achievements> achievements;
+    private static AchievementList instance;
+
+    private AchievementList() {
+        achievements = DataLoader.loadAchievements();
+    }
+
+    public static AchievementList getInstance() {
+        if(instance == null) {
+            instance = new AchievementList();
+        } 
+        return instance;
+    }    
+
+    // Method to add a achievement to the list
+    public void addAchievement(Achievements ach) {
+        if (ach != null && !achievements.contains(ach)) {
+            achievements.add(ach);
+        }
+    }
+
+
+
+
+
     
 }
