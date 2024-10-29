7c286049a895baa50b718f77b0cc105ff34afe76
CamronMartin <cbrycem02@gmail.com>
2024-10-29 14:32:02 -0400

    Have a working JUnit test file.

---
+++ speek/src/test/java/model/AchieveTest.java
@@ -0,0 +1,15 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.*;
+import org.junit.jupiter.*;
+
+public class AchieveTest {
+  @Test
+  public void myTest() {
+    assertTrue(true);
+  }
+}
+
